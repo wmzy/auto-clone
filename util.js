7b7ed5e0cacee6526f1b48870b8ffc0469937541
wmzy <1256573276@qq.com>
2020-04-27 13:26:32 +0800

    fix: tilde path support

--- util.js
+++ util.js
@@ -1,6 +1,9 @@
 const {execSync} = require('child_process');
+  const untildify = require('untildify');
 
 exports.execAt = function execAt(cwd) {
+  if (cwd) cwd = untildify(cwd);
+
   return command => {
     console.log(command);
     execSync(command, {stdio: 'inherit', cwd});
